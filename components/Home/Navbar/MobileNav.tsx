c33e3ed7a3ffcf0f5ec77ceff54453fe8fbf9085
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-23 06:29:25 +0300

    MobileNav > overlay

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -1,5 +1,15 @@
 const MobileNav = () => {
-   return <div></div>;
+   return (
+      <div>
+         {/* OVERLAY */}
+         <div
+            className="fixed inset-0 transition-all duration-500
+            z-[100002] bg-black opacity-70"
+         ></div>
+
+         {/* NAVLINKS */}
+      </div>
+   );
 };
 
 export default MobileNav;
