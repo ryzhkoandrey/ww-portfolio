9761a4baa425074c1d38c052d161483b1969d9dc
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-08-25 21:37:07 +0300

    Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
+import Footer from '@/components/Home/Footer/Footer';
 
 const font = Inter({
    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -23,6 +24,7 @@ export default function RootLayout({
          <body className={`${font.className} antialiased bg-[#0d0d1f]`}>
             <ResponsiveNav />
             {children}
+            <Footer />
          </body>
       </html>
    );
---
+++ components/Home/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+   return (
+      <footer className="bg-blue-900/20 p-5">
+         <p className="text-lg text-white/70 text-center">
+            Sajal - Personal Portfolio Website
+         </p>
+      </footer>
+   );
+};
+
+export default Footer;
